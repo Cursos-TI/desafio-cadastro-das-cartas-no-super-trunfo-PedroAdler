e11657b7fd6d90b850652ab85615f624d98a0860
PedroAdler <pedroadler88@gmail.com>
2025-02-18 01:40:23 +0000

    codigo trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,15 +7,12 @@
 //Teste larissa
 
 int main() {
-    //carta1
+
+    //carta1:
     
-     char estado = 'C';
-     char codigo[3] = "C01";
-     char nome_da_cidade = "Fortaleza";
-     float populacao = 2429678;
-     float area = 313.8;
-     float pib = 7343612800000;
-     int pontos_turisticos = 20;
+     
+
+
 
 
 
