7bd9bede63244685483dfad7d2df7dfa961a597a
PedroAdler <pedroadler88@gmail.com>
2025-02-18 01:47:09 +0000

    meu codigo trunfo aas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,7 +10,52 @@ int main() {
 
     //carta1:
     
-     
+    
+    char estado = 'C';
+    char codigo[4] = "C01";
+    char nome_da_cidade[20] = "Fortaleza";
+    float populacao = 2429678;
+    float area = 313.8;
+    double pib = 7343612800000;
+    int pontos_turisticos = 20;
+
+    //Carta2:
+
+    
+    char estado2 = 'B';
+    char codigo2[3] = "B02";
+    char nome_da_cidade2[21] = "Salvador";
+    float populacao2 = 14.14;
+    float area2 = 567.295;
+    double pib2 = 114.4;
+    int pontos_turisticos2 = 15;
+
+
+    
+    printf("Estado: %c\n", estado);
+    printf("codigo: %s\n", codigo);
+    printf("Nome da cidade: %s\n", nome_da_cidade);
+    printf("população: %f\n", populacao);
+    printf("Area: %f\n", area);
+    printf("pib: %f\n", pib);
+    printf("numero de pontos turisticos: %d\n", pontos_turisticos);    
+
+
+    printf("Estado: %c\n", estado2);
+    printf("codigo: %s\n", codigo2);
+    printf("nome da cidade: %s\n", nome_da_cidade2);
+    printf("população: %f\n", populacao2);
+    printf("Area: %f\n", area2);
+    printf("pib: %f\n", pib2);
+    printf("quantidade de pontos turisticos: %d\n", pontos_turisticos2);
+
+
+
+
+
+
+
+    return 0;
 
 
 
